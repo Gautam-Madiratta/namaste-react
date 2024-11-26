a678492780d12d3c49ed6e7599c1d9152635bf9f
Gautam <gautam.madiratta2109@gmail.com>
2024-11-26 18:46:37 +0530

    EP-02

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-const object = React.createElement(
-    "div",
-    { id: "parent" }, [
-    React.createElement(
-        "div",
-        { id: "child" },
-        [React.createElement("h1", {}, "This is H1 Tag"),
-        React.createElement("h2", {}, "This is H2 Tag")] //this is how you create a sibling
-    ),
-    React.createElement(
-        "div",
-        { id: "child" },
-        [React.createElement("h1", {}, "This is H1 Tag"),
-        React.createElement("h2", {}, "This is H2 Tag")]
-    )])
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-//it becomes very difficult to manage and its untidy to maintain    
+const object = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child" },
+    [
+      React.createElement("h1", {}, "This is H1 Tag"),
+      React.createElement("h2", {}, "This is H2 Tag"),
+    ] //this is how you create a sibling
+  ),
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is H1 Tag"),
+    React.createElement("h2", {}, "This is H2 Tag"),
+  ]),
+]);
+
+//it becomes very difficult to manage and its untidy to maintain
 
 const object1 = React.createElement("h1", { id: "text" }, "Hello from react");
 
